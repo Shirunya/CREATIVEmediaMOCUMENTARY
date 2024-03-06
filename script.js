78b63d04c9c400a88342035fae79fa59fbe22cdc
Iryna Shyshko <484670@LG203MAC06.local>
2024-03-06 11:53:32 +0000

    button moved

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-alert("YOU ARE TO ENTER DEAD ZONE");
-confirm("Wear your PROTECTIVE GEAR at ALL TIMES");
-var age = prompt("ENTER YOUR AGE");
 
-if (age >= 18) {
-  alert("You can start the game NOW!");
-  confirm("START GAME")
-} 
-else {
-  alert("Sorry, you are not old enough. GOOD BYE!");
+function starting() {
+  function changeVisibility() {
+    document.getElementById("start").style.visibility = "hidden";
+
+  }
+  changeVisibility()
+  alert("YOU ARE TO ENTER DEAD ZONE");
+  confirm("Wear your PROTECTIVE GEAR at ALL TIMES");
+  var age = prompt("ENTER YOUR AGE");
+  
+  if (age >= 18) {
+    alert("You can start the game NOW!");
+    confirm("START GAME")
+  } 
+  else {
+    alert("Sorry, you are not old enough. GOOD BYE!");
+  }
+  const newButton = document.createElement('button');
+  newButton.textContent = 'START GAME';
+  document.body.appendChild(newButton);
+  
+  newButton.addEventListener('click', () => {
+    alert('GAME OVER........R.I.P');
+  });
 }
-const newButton = document.createElement('button');
-newButton.textContent = 'START GAME';
-document.body.appendChild(newButton);
-
-newButton.addEventListener('click', () => {
-  alert('GAME OVER........R.I.P');
-});
